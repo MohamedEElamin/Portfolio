fa944602b1705daeda997eddc6dd4d9730ee2fa8
MohamedEElamin <61060658+MohamedEElamin@users.noreply.github.com>
2025-10-26 10:51:35 -0500

    add moble menu

--- script.js
+++ script.js
@@ -33,3 +33,11 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
